8d38d5216ccf4d4c16d7c294a1345ad7b50be2c1
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-04-10 16:37:10 +0500

    Delete words.py

--- Assignments_01_09_projects/words.py
+++
@@ -1,6 +0,0 @@
-words = [
-    'python', 'programming', 'computer', 'algorithm', 'database',
-    'network', 'software', 'hardware', 'internet', 'developer',
-    'coding', 'debugging', 'testing', 'deployment', 'version',
-    'control', 'repository', 'function', 'variable', 'class'
-] 
